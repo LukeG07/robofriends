af8dd98cec3b5b7b205af8699f49ace74afb859f
Luke Gibbard <lukeg@iotechsys.com>
2020-06-21 16:23:05 +0100

    Initial commit

---
+++ build/precache-manifest.0059e2d5a09040f41677117c1015f668.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9a35c209694d51a42adc625a9e606e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "64544ef61b581fb06efb",
+    "url": "/static/css/main.6f69e27f.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64544ef61b581fb06efb",
+    "url": "/static/js/main.5c5b73b8.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
